b7b42524e14b86d299561faa41892c0b4703db60
Anh Huy Ngo <49578740+emtrai@users.noreply.github.com>
2024-10-13 20:57:25 +0700

    download multiple threads

--- README.md
+++ README.md
@@ -1,21 +1,54 @@
-Download multiple file at once
+Download multiple files
 
+This small tool, a python code, can be used to download multiple files.
+
+List of urls to download can be specified in a file, each line of file is URL to download a file, i.e.
+```
+https://github.com/verigak/progress/blob/master/test_progress.py
+```
+
+
+> **_NOTE:_**  For Chrome user, use [Link Grabber extension](https://chromewebstore.google.com/detail/link-grabber/caodelkhipncidmoebgbbeemedohcdma) to get multiple download links of a webpage
+
+# REQUIREMENT
+
+python3 with following package installed
+- tqdm
 
 # USAGE
 
 ```
 usage: Download file [-h] [--fileurl FILEURL] [--outdir OUTDIR] [--url URL]
+                     [-j JOB] [--untrusted]
 
-Download multiple files
+Download multiple files in seperated threads/tasks
 
 optional arguments:
   -h, --help         show this help message and exit
   --fileurl FILEURL  File contains list of url to be download
-  --outdir OUTDIR    Output directory
-  --url URL          URl to download file
+  --outdir OUTDIR    Output directory, default
+                     '/Users/worker/work/github/download_files/.download'
+  --url URL          URl to download file, multiple URLs can be separated by
+                     ','
+  -j JOB, --job JOB  The number of jobs/threads to download. Default is a haft
+                     of CPU cores, i.e. 2
+  --untrusted        By-pass untrusted URL (skip SSL failure)
 
 Copyright @ 2024 Ngo Huy Anh
 ```
+
+# EXAMPLE
+
+```
+./download_file.py --untrusted --url https://raw.githubusercontent.com/emtrai/download_files/refs/heads/main/README.md,https://raw.githubusercontent.com/emtrai/download_files/refs/heads/main/download_file.py
+```
+
+
+# TODO
+
+- Download file in multi parts
+- Parse webpage to get download links and download
+
 
 # COPYRIGHT
 
--- download_file.py
+++ download_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Copyright [2024] Ngo Huy Anh
 #
@@ -13,6 +13,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Download multiple files
+# Author: Anh, Ngo - ngohuyanh@gmail.com
+#
+
 
 from urllib.request import urlopen
 from shutil import copyfileobj
@@ -22,15 +27,32 @@ import sys
 import logging
 import html
 from urllib.parse import unquote
+import urllib
+import ssl
+import threading
+from tqdm import tqdm
+import multiprocessing
 
 logger = logging.getLogger(__name__)
 
 CUR_DIR = os.getcwd()
 DEFAULT_OUT_DIR=os.path.join(CUR_DIR, ".download")
 
+
 COMMENT_CHAR="#"
 
+LOG_FNAME="download.log"
 
+URL_SPLIT=","
+    
+LIST_URLS = []
+NEXT_URL_IDX = 0
+TOTAL_URLS = 0
+TOTAL_DOWNLOADED =0
+NUMBER_CORES = multiprocessing.cpu_count()
+DEFAULT_JOBS = NUMBER_CORES/2 if NUMBER_CORES > 1 else 1
+
+LOG_FILE=None
 
 class UrlItem(object):
     _url = None
@@ -44,6 +66,7 @@ class UrlItem(object):
             self._fname = fname.strip()
         else:
             parts = self._url.split("/")
+            # get filename from url
             for part in reversed(parts):
                 part = part.strip()
                 if len(part) > 0:
@@ -61,38 +84,161 @@ class UrlItem(object):
     
     def __str__(self):
         return "%s --> %s" % (self.url, self.fname)
+
+class DownloadProgressBar():
+    _bar = None
+    _running = False
+    _title = None
+    _line_offset = 0
+    
+    def __init__(self, line_offset = 0):
+        self._line_offset = line_offset
+        self._bar = tqdm()
         
+    def set_title(self, title):
+        self._title = title
+
+    def start(self, max_value):
+        maxval=max_value
+        if maxval < 0:
+            maxval=-1
+        self._running = True
+        if self._bar is not None:
+            self._bar.reset(max_value)
+            self._bar.clear()
+            self._bar.set_description(self._title)
+        
+    def update(self, value):
+        if self._bar is not None and self._running:
+            self._bar.update(value)
+            
+    def end(self):
+        self._running = False
+        if self._bar is not None:
+            self._bar.reset()
+            self._bar.clear()
+
+    def __call__(self, num_block, block_size, total_size):
+        if (total_size != 0):
+            if (not self._running):
+                self.start(total_size)
+
+            downloaded = num_block * block_size
+            if downloaded < total_size:
+                self.update(block_size)
+            else:
+                self.end()
+            
+def append_log(log_msg):
+    global LOG_FILE
+    if log_msg is not None and len(log_msg) > 0:
+        with open(LOG_FILE, 'a') as log:
+            log.write("%s\n" % log_msg)
+        log.close()
+
 
 def normalize_fname(fname):
-    # fname.replace("%20", "_")
     fname = html.unescape(fname)
     fname = unquote(fname)
     return fname
 
-def download_file(url, fpath):
-    logger.info("Download file from %s" % (url))
-    logger.info("Store to %s" % (fpath))
-    with urlopen(url) as istream:
-        with open(fpath, 'wb') as ofile:
-            copyfileobj(istream, ofile)
+def get_fpath(outdir, fname):
+    fpath = None
+    suffix = None
+    for i in range(100):
+        fpath = os.path.join(outdir, fname)
+        # Try to find suitable file name, if it exists, add "(number)"
+        if (os.path.exists(fpath)):
+            parts = os.path.splitext(fpath)
+            fname_no_ex = parts[0]
+            if suffix is not None and parts[0].endswith(suffix):
+               fname_no_ex = fname_no_ex[:len(fname_no_ex) - len(suffix)]
+            suffix = "(%s)" % i
+            fname = "%s%s%s" % (fname_no_ex, suffix, parts[1])
+        else:
+            break
+    return fpath
 
+def download_file(title, url, fpath, bar):
+    '''
+    Download file
+    '''
+    # TODO: Download multi parts of file
+    bar.set_title(title)
+    urllib.request.urlretrieve(url, fpath, bar)
+
+def download_thread(thread_id, lock, outdir, bar):
+    '''
+    Download thread
+    
+    @param lock: synchornize lock
+    @param outdir: directory to store download file    
+    @param bar: Progress bar
+    '''
+    global NEXT_URL_IDX
+    global LIST_URLS
+    global TOTAL_URLS
+    global TOTAL_DOWNLOADED
+    
+    
+    while True:
+        url_item = None
+        
+        # get next index of URL to download
+        lock.acquire() 
+        if (NEXT_URL_IDX < len(LIST_URLS)):
+            url_item = LIST_URLS[NEXT_URL_IDX]
+            NEXT_URL_IDX += 1
+        lock.release() 
+        
+        if (url_item is None):
+            break
+        
+        
+        fpath = get_fpath(outdir, url_item.fname)
+        log_msg = None
+        
+        # download file
+        if (fpath is not None):
+            file_name = os.path.basename(fpath)
+            download_file("[%d] %s" % (thread_id, file_name), url_item.url, fpath, bar)
+            lock.acquire()
+            TOTAL_DOWNLOADED += 1 
+            log_msg = "[%d] Done: %s" % (TOTAL_DOWNLOADED, str(url_item))
+            lock.release()
+        else:
+            logger.error("FAILED to get file path for URL '%s', file name '%s'" % (url_item.url, url_item.fname))
+            lock.acquire()
+            TOTAL_DOWNLOADED += 1 
+            log_msg = "[%d] Error: %s" % (TOTAL_DOWNLOADED, str(url_item))
+            lock.release()
+
+        append_log(log_msg)
+
+    logger.debug("Stop thread %d" % thread_id)
 
 def parse_one_url(url):
+    '''
+    Convert url to UrlItem for downloading
+    '''
     url_item = UrlItem(url)
     logger.debug("parsed url '%s'" % str(url_item))
     return url_item
 
-def parse_urls(urls):
-    logger.debug("Get list of URL from '%s'" % urls)
-    # TODO: parse list of URL from commandline, i.e pslit by comma, or anything else
-    url = parse_one_url(urls)
-    return [url]
-
 
 def get_urls_from_file(fpath):
+    '''
+    Get List of URLs from file
+    
+    @return list of UrlItem object
+    '''
     urls = []
     logger.debug("Get list of URL from file '%s'" % fpath)
     lines = None
+    if fpath is None or len(fpath) == 0 or not os.path.exists(fpath):
+        logger.error("Invalid file")
+        return urls
+    
     with open(fpath, 'r') as file:
         lines = file.readlines()
     for line in lines:
@@ -103,65 +249,96 @@ def get_urls_from_file(fpath):
 
 def main(args):
     logger.info("Output dir '%s'" % args.outdir)
+    if args.untrusted:
+        ssl._create_default_https_context = ssl._create_unverified_context
+
     if not os.path.exists(args.outdir):
         logger.info("Create output dir '%s'" % args.outdir)
         os.makedirs(args.outdir)
+      
     
-    list_urls = []
+    global LIST_URLS
+    global NEXT_URL_IDX
+    global TOTAL_URLS
+    global LOG_FILE
+    
+    LOG_FILE=os.path.join(args.outdir, LOG_FNAME)
+    
+    NEXT_URL_IDX = 0
+    LIST_URLS = []
+    
+    # get list of URL from argument
     if args.url is not None and len(args.url) > 0:
-        urls = parse_urls(args.url)
-        if (urls is not None and len(urls) > 0):
-            list_urls += urls
+        parts = args.url.split(URL_SPLIT)
+        for part in parts:
+            part = part.strip()
+            if len(part) == 0:
+                continue
+            url = parse_one_url(part)
+            if url is not None:
+                LIST_URLS.append(url)
     
+    # get list of URL from file
     if args.fileurl is not None and len(args.fileurl) > 0:
         if os.path.exists(args.fileurl):
             urls = get_urls_from_file(args.fileurl)
             if (urls is not None and len(urls) > 0):
-                list_urls += urls
+                LIST_URLS += urls
         else:
             raise Exception("File '%s' does not exist" % args.fileurl)
     
-    logger.debug("List of url: \n" + "\n".join(str(url) for url in list_urls))
+    logger.debug("List of url: \n" + "\n".join(str(url) for url in LIST_URLS))
     
-    skip_url=[]
+    TOTAL_URLS = len(LIST_URLS)
     
-    for url in list_urls:
-        logger.info("\nDOWNLOAD FILE from '%s' ......." % url.url)
-        fpath = None
-        fname = url.fname
-        for i in range(100):
-            fpath = os.path.join(args.outdir, fname)
-            if (not os.path.exists(fpath)):
-                break
-            else:
-                parts = os.path.splitext(fpath)
-                fname = "%s(%d)%s" % (parts[0], i, parts[1])
-        if (fpath is not None):
-            download_file(url.url, fpath)
-        else:
-            logger.error("FAILED to download from '%s'. Not found and suitable file to download" % url.url)
-            skip_url.append(url)
-    if len(skip_url) > 0:
-        logger.info("\nSKIP TO DOWNLOAD FROM FOLLOWING URLS:\n")
-        for url in skip_url:
-            logger.info(str(url))
+    # init log
+    with open(LOG_FILE, 'w') as log:
+        log.write("Total URLs: %d\n" % TOTAL_URLS)
+    
+    num_jobs = int(args.job)
+    if (num_jobs > TOTAL_URLS):
+        num_jobs = TOTAL_URLS
+    
+    lock = threading.Lock()
+    run_threads = []
+
+    logger.info("Create %d job(s) to download" % num_jobs)
+
+    # Create thread to download
+    for job in range(num_jobs):
+        run_thread = threading.Thread(target=download_thread, args=(job, lock,args.outdir, DownloadProgressBar(job))) 
+        run_threads.append(run_thread)
+        run_thread.start()
+    
+    # wait for threads complete
+    for t in run_threads:
+        t.join()
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(
                     prog='Download file',
-                    description='Download multiple files',
+                    description='Download multiple files in seperated threads/tasks',
                     epilog='Copyright @ 2024 Ngo Huy Anh')
+    
     parser.add_argument('--fileurl', action='store',
                         help='File contains list of url to be download')
+    
     parser.add_argument('--outdir', action='store',
                         default=DEFAULT_OUT_DIR,
-                        help='Output directory')
+                        help="Output directory, default '%s'" % DEFAULT_OUT_DIR)
     
     parser.add_argument('--url', action='store',
-                        help='URl to download file')
+                        help="URl to download file, multiple URLs can be separated by '%s'" % URL_SPLIT)
+    
+    parser.add_argument('-j', '--job', action='store', type=int, default=DEFAULT_JOBS,
+                        help="The number of jobs/threads to download. Default is a haft of CPU cores, i.e. %d" % DEFAULT_JOBS)
+    
+    parser.add_argument('--untrusted', action='store_true', default=False,
+                        help='By-pass untrusted URL (skip SSL failure)')
 
     args = parser.parse_args()
     
-    logging.basicConfig(level=logging.DEBUG)  
+    logging.basicConfig(level=logging.INFO)  
     main(args)
